dde3c1cc8643053d3dd4415d124c433f364f1659
Vano Maisuradze <vano@maisuradze.ge>
2019-10-31 12:05:02 +0400

    updated demo

--- ChemicalEquationBalancer.Demo/Program.cs
+++ ChemicalEquationBalancer.Demo/Program.cs
@@ -6,30 +6,23 @@ namespace ChemicalEquationBalancer.Demo
     {
         static void Main(string[] args)
         {
-            //var inputs = new[] 
-            //{
-            //    "H2 + O2 = H2O",
-            //    "O2 + FeS2 = SO2 + Fe2O3",
-            //    "H2O2 + Cr2O7^2- = Cr^3+ + O2 + OH^-",
-            //    "K4Fe(CN)6 + KMnO4 + H2SO4 = KHSO4 + Fe2(SO4)3 + MnSO4 + HNO3 + CO2 + H2O"
-            //};
-            //var balancer = new Balancer();
-
-            //foreach (var input in inputs)
-            //{
-            //    balancer.Solve(input);
-            //    Console.WriteLine(balancer.EquationHtml);
-            //    Console.WriteLine($" Input: {input}");
-            //    Console.WriteLine($"Output: {balancer.EquationText}");
-            //    Console.WriteLine();
-            //}
-
-            var input = "H2 + O2 = H2O";
+            var inputs = new[] 
+            {
+                "H2 + O2 = H2O",
+                "O2 + FeS2 = SO2 + Fe2O3",
+                "H2O2 + Cr2O7^2- = Cr^3+ + O2 + OH^-",
+                "K4Fe(CN)6 + KMnO4 + H2SO4 = KHSO4 + Fe2(SO4)3 + MnSO4 + HNO3 + CO2 + H2O"
+            };
             var balancer = new Balancer();
-            balancer.Solve(input);
-            Console.WriteLine(balancer.EquationHtml);
-            Console.WriteLine($" Input: {input}");
-            Console.WriteLine($"Output: {balancer.EquationText}");
+
+            foreach (var input in inputs)
+            {
+                balancer.Solve(input);
+                //Console.WriteLine(balancer.EquationHtml);
+                Console.WriteLine($" Input: {input}");
+                Console.WriteLine($"Output: {balancer.EquationText}");
+                Console.WriteLine();
+            }
 
             Console.ReadLine();
         }
